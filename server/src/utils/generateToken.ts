06a5a3ba0bef1ead35912ae42ec80a3777ec1e85
ericericmoura <eric.moura.dev@gmail.com>
2026-09-22 16:01:48 -0300

    feat: implement generate token functionality, all tests passing

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,3 +1,19 @@
+import jwt from "jsonwebtoken";
+import env from "@/config/env.js"
+import type { StringValue } from "ms";
+
 export const generateToken = (payload: object, expirationInMinutes: string): string => {
-    throw Error("not implemented");
+    const exp = expirationInMinutes as StringValue;
+
+    if (!exp)
+    {
+        throw Error("invalid expiration time.");
+    }
+
+    const token = jwt.sign(payload, env.JWT_SECRET, {
+        issuer: env.JWT_ISSUER,
+        expiresIn: exp,
+    })
+
+    return token;
 }
--- server/tests/unit/utils/generateToken.test.ts
+++ server/tests/unit/utils/generateToken.test.ts
@@ -2,6 +2,7 @@ import env from "@/config/env.js"
 import jwt from "jsonwebtoken"
 
 import { generateToken } from "@/utils/generateToken.js";
+import ms from "ms";
 
 type TokenPayload = jwt.JwtPayload & { id: number };
 
@@ -10,15 +11,15 @@ describe('Token Generation', () => {
 
     beforeAll(() => {
         token = generateToken({ id: 20 }, env.LOGIN_TOKEN_EXPIRATION);
-        expect(token).toBeDefined();        
+        expect(token).toBeDefined();
     })
 
-    test('correctly generates token', () => {        
+    test('correctly generates token', () => {
         const decoded = jwt.verify(token, env.JWT_SECRET, {
             issuer: env.JWT_ISSUER
         }) as TokenPayload;
 
-        expect(decoded.id ).toBe(20);
+        expect(decoded.id).toBe(20);
         expect(decoded.iss).toBe(env.JWT_ISSUER);
     });
     test('rejects token with wrong secret', () => {
@@ -36,10 +37,17 @@ describe('Token Generation', () => {
         }).toThrow();
     })
     test('correctly sets token expiration date', () => {
+        const date = Date.now();
+        jest.spyOn(Date, 'now').mockReturnValue(date);
+
         const decoded = jwt.verify(token, env.JWT_SECRET, {
             issuer: env.JWT_ISSUER
         }) as TokenPayload;
-        
-        expect(decoded.exp).toBe("15m");
+
+        const expectedExp = Math.floor((date + ms(env.LOGIN_TOKEN_EXPIRATION as ms.StringValue)) / 1000);
+        expect(decoded.exp).toBe(expectedExp);
+    })
+    test('throw error for invalid expiration', () => {
+        expect(() => generateToken({ id: 20 }, "384218uo")).toThrow();
     })
 })
