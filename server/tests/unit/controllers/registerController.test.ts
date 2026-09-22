14f27f1d884cd9cdf9ae69f068d618334bdecc23
ericericmoura <eric.moura.dev@gmail.com>
2026-09-22 16:11:59 -0300

    test: add user register test declarations

---
+++ server/tests/unit/controllers/registerController.test.ts
@@ -0,0 +1,8 @@
+describe("Register User", () => {
+    test.todo("rejects a request with invalid or missing fields")
+    test.todo("rejects a request with a weak password")
+    test.todo("rejects a request with an already existing e-mail")
+    test.todo("rejects a request with an already existing username")
+    test.todo("creates and returns a new user on success")
+    test.todo("returns a valid JWT token on success")
+})
