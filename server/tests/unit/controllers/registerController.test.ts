ce7daa187721172024869cea917a9b10d95b3f1f
ericericmoura <eric.moura.dev@gmail.com>
2026-09-22 19:22:08 -0300

    refactor: move helpers to the end of the test describe

--- server/tests/unit/controllers/registerController.test.ts
+++ server/tests/unit/controllers/registerController.test.ts
@@ -21,39 +21,8 @@ describe("Register User", () => {
         res = httpMocks.createResponse();
     })
 
-    const mockFindUser = (body: RegisterBody) => {
-        prismaMock.user.findUnique.mockResolvedValueOnce({
-            id: 1,
-            email: body.email ?? "testuser@test.com",
-            username: body.username ?? "testuser1",
-            passwordHash: "@MyStrongPassword123",
-            isActive: true,
-            role: Roles.USER,
-            confirmedEmail: true,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        });
-    }
-
-    const expectRejection = (statusCode = 400) => {
-        expect(next).toHaveBeenCalledWith(expect.objectContaining({ statusCode }));
-        expect(prismaMock.user.create).not.toHaveBeenCalled()
-    }        
-
-    const createRegisterRequest = (body: RegisterBody) => {
-        return httpMocks.createRequest({
-            method: "POST",
-            baseUrl: "/register",
-            body: {
-                email: body.email ?? "testuser@test.com",
-                username: body.username ?? "testuser123",
-                password: body.password ?? "@StrongPassword123",
-            }
-        });
-    }
-
     test("rejects a request with invalid or missing fields", async () => {
-        const req = createRegisterRequest({email: "", username: ""});
+        const req = createRegisterRequest({email: "321", username: ""});
 
         await registerController(req, res, next);
         
@@ -90,4 +59,37 @@ describe("Register User", () => {
 
     test.todo("creates a new user on success")
     test.todo("returns a valid JWT token on success")
+
+    
+    // TEST HELPERS
+    const mockFindUser = (body: RegisterBody) => {
+        prismaMock.user.findUnique.mockResolvedValueOnce({
+            id: 1,
+            email: body.email ?? "testuser@test.com",
+            username: body.username ?? "testuser1",
+            passwordHash: "@MyStrongPassword123",
+            isActive: true,
+            role: Roles.USER,
+            confirmedEmail: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+    }
+
+    const expectRejection = (statusCode = 400) => {
+        expect(next).toHaveBeenCalledWith(expect.objectContaining({ statusCode }));
+        expect(prismaMock.user.create).not.toHaveBeenCalled()
+    }
+
+    const createRegisterRequest = (body: RegisterBody) => {
+        return httpMocks.createRequest({
+            method: "POST",
+            baseUrl: "/register",
+            body: {
+                email: body.email ?? "testuser@test.com",
+                username: body.username ?? "testuser123",
+                password: body.password ?? "@StrongPassword123",
+            }
+        });
+    }
 })
