e0ce546c6812ea3c86ad11d6719e286b14caaeb2
ericericmoura <eric.moura.dev@gmail.com>
2026-09-23 11:58:40 -0300

    feat: implement register controller and token verification

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,4 +1,54 @@
 import type { Request, Response, NextFunction } from "express";
-import { prisma } from "@/config/database";
+import createHttpError from "http-errors";
+import bcrypt from "bcrypt"
 
-export const registerController = async (req: Request, res: Response, next: NextFunction) => {}
+import { prisma } from "@/config/database.js";
+import type { Roles } from "@root/prisma/generated/prisma/enums.js";
+import { generateToken } from "@/utils/generateToken.js";
+import env from "@/config/env.js"
+
+interface RegisterBody {
+    email: string
+    username: string
+    password: string
+}
+
+interface UserDto {
+    email: string
+    username: string,
+    confirmedEmail: boolean,
+    role: Roles,
+    isActive: boolean
+}
+
+export const registerController = async (
+    req: Request<{}, {}, RegisterBody>,
+    res: Response,
+    next: NextFunction) => {
+    const { email, username, password } = req.body
+
+    const foundUser = await prisma.user.findFirst({
+        where: { OR: [{ email }, { username }] }
+    });
+    if (foundUser) {
+        return next(createHttpError(400, "e-mail or username already in use."));
+    }
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    
+    try {
+        const user = await prisma.user.create({data: {
+            email,
+            passwordHash,
+            username
+        }})    
+    
+        const token = generateToken({id: user.id, role: user.role}, env.LOGIN_TOKEN_EXPIRATION);
+    
+        res.status(201).json({ data: user as UserDto, token });
+    } catch (error) {
+        console.log(`error: ${error}`);
+        return next(createHttpError(500, "Error: " + error));
+    }
+}
--- server/src/utils/verifyToken.ts
+++ server/src/utils/verifyToken.ts
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
+import env from "@/config/env.js"
 
-export const verifyToken = (token: string): jwt.JwtPayload {
-    throw Error("not implemented.");
+export const verifyToken = (token: string): jwt.JwtPayload => {
+    const decoded = jwt.verify(token, env.JWT_SECRET, {issuer: env.JWT_ISSUER});
+    return decoded as jwt.JwtPayload;
 }
