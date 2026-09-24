6ec775962b9d7069cc8996b91a66d73853084474
ericericmoura <eric.moura.dev@gmail.com>
2026-09-24 11:55:39 -0300

    fix: database configuration with validation for connection string

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,10 +1,8 @@
-import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@root/prisma/generated/prisma/client";
+import env from "@/config/env.js"
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
