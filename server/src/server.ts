dbff0ab49a5a985acec86fadf2106c9958f42b6c
ericericmoura <eric.moura.dev@gmail.com>
2026-09-25 16:26:55 -0300

    feat: add https using a self signed SSL/TLS certificate

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,36 +3,45 @@ import express, {type Request, type Response} from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import "express-async-errors"
+import https from "https"
+import fs from "fs";
 
 import apiV1Router from '@/routes/apiV1.js';
 import "@/middlewares/zodErrorHandler.js"
 
 export const app = express();
 
-// view engine setup
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// API Routes
 app.use('/api/v1', apiV1Router);
 
-// catch 404 and forward to error handler
+// 404 handler
 app.use(function (req, res, next) {
   next(createHttpError(404, "endpoint could not be found."));
 });
 
-// error handler
+// Global error handler
 app.use(function (err: HttpError, req: Request, res: Response) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log("App listening on http://localhost:3000.");
+// HTTPS Configuration
+const privateKey = fs.readFileSync('certificate/server.key', 'utf8');
+const certificate = fs.readFileSync('certificate/server.crt', 'utf8');
+
+var credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3000, () => {
+  console.log("App listening on https://localhost:3000.");
 })
